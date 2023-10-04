7c72fa183b07c632f1f3ff8e12960e5886b195d5
satyam <satyam@gmail.com>
2023-10-04 10:33:49 +0530

    created user model

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
 
 const app = express();
 
 const connectToMongoDB = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/real-estate");
-        console.log("connected to mongodb");
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect("mongodb://localhost:27017/real-estate");
+    console.log("connected to mongodb");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 connectToMongoDB();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
