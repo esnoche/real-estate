9464c6113d09c40335f4f538ae0efe8838c44283
satyam <satyam@gmail.com>
2023-10-04 10:22:37 +0530

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express"
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(3000,() => {
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/real-estate");
+        console.log("connected to mongodb");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+connectToMongoDB();
+
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
