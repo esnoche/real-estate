ce42ab1ee36949a98de7c7d6b9d646a4d5845fdd
satyam <satyam@gmail.com>
2023-10-05 12:39:57 +0530

    created signin api route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { userName, email, password } = req.body;
@@ -16,3 +18,33 @@ export const signup = async (req, res, next) => {
     next(error); //next(errorHandler);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return next(errorHandler(404, "User NotFound"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, existingUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Password"));
+    }
+
+    const token = jwt.sign({id: existingUser._id}, "jjk");// similar as salting
+
+    const {password: pass, ...rest} = existingUser._doc;// do not want to send the password to client
+    
+    res.cookie("access_token", token, {httpOnly: true}).status(200).json(rest);
+
+  } catch (error) {
+
+    next(error);
+
+  }
+}
