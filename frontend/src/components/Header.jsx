acd272dc59b1466a8667c84ea776710fd1e3cd48
satyam <satyam@gmail.com>
2023-10-06 14:37:25 +0530

    updated header and profile page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { FaSearch } from "react-icons/fa"
 import { Link } from 'react-router-dom'
+import { useSelector } from "react-redux"
 
 export default function Header() {
+    const { currentUser } = useSelector(state => state.user);
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -23,9 +25,16 @@ export default function Header() {
                     <Link to={"/about"}>
                         <li className='hidden sm:inline text-slate-800 hover:text-slate-600'>About</li>
                     </Link>
-                    <Link to={"/sign-in"}>
-                        <li className='text-slate-800 hover:text-slate-600'>Sign In</li>
-                    </Link>
+                    {currentUser ? (
+                        <Link to={"/profile"}>
+                            <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" />
+                        </Link>
+                    ) : (
+
+                        <Link to={"/sign-in"}>
+                            <li className='text-slate-800 hover:text-slate-600'>Sign In</li>
+                        </Link>
+                    )}
                 </ul>
             </div>
         </header>
