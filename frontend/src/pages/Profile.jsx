970f009ed8298190dd5ea20144015ad16beeb7f0
satyam <satyam@gmail.com>
2023-10-12 01:56:03 +0530

    created show-user-listings functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { updateUserStart, updateUserSuccess, updateUserFailure, deleteAccountFai
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
 import { useDispatch, useSelector } from 'react-redux'
 import React, { useEffect, useState } from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import { app } from '../firebase';
 import { useRef } from 'react';
 
@@ -10,8 +10,10 @@ export default function Profile() {
 
   const [fileUploadError, setFileUploadError] = useState(false);
   const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [listingsError, setListingsError] = useState(false);
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [uploadProgress, setUploadProgress] = useState(0);
+  const [userListings, setUserListings] = useState([]);
   const [formData, setFormData] = useState({});
   const [file, setFile] = useState(undefined);
   const filerRef = useRef(null);
@@ -80,7 +82,7 @@ export default function Profile() {
     }
   };
 
-  const handleDeleteAccount = async (req, res) => {
+  const handleDeleteAccount = async () => {
     try {
       dispatch(deleteAccountStart());
 
@@ -97,7 +99,7 @@ export default function Profile() {
     }
   };
 
-  const handleSignOut = async (req, res) => {
+  const handleSignOut = async () => {
     try {
       dispatch(signOutStart());
 
@@ -111,6 +113,21 @@ export default function Profile() {
       dispatch(signOutSuccess(data));
     } catch (error) {
       dispatch(signOutFailure(error.message));
+    }
+  };
+
+  const handleShowListings = async () => {
+    try {
+      setListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setListingsError(true);
     }
   }
 
@@ -162,7 +179,7 @@ export default function Profile() {
 
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-85'>{loading ? "UPDATING.." : "UPDATE"}</button>
 
-        <Link to={"/create-listing" } className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover: opacity-95'>Create Listing</Link>
+        <Link to={"/create-listing"} className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover: opacity-95'>Create Listing</Link>
 
       </form>
 
@@ -171,6 +188,30 @@ export default function Profile() {
         <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
 
+      <button onClick={handleShowListings} className='text-green-700 w-full '>Show Listings</button>
+      <p className='text-red-700 mt-5'>{listingsError ? "Error while getting listings" : ""}</p>
+      {userListings && userListings.length > 0 &&
+        <div className='fflex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+          {
+            userListings.map((listing) =>
+              <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+                <Link to={`/listing/${listing._id}`}>
+                  <img src={listing.imageUrls[0]} alt="cover image" className='h-16 w-16 object-contain ' />
+                </Link>
+                <Link to={`/listing/${listing._id}`} className='flex-1 text-slate-700 font-semibold hover:opacity-90 truncate'>
+                  <p>{listing.name}</p>
+                </Link>
+                <div className='flex flex-col items-center'>
+                  <button className='text-red-700 uppercase'>Delete</button>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </div>
+
+              </div>
+            )
+          }
+        </div>
+      }
     </div>
   )
 }
